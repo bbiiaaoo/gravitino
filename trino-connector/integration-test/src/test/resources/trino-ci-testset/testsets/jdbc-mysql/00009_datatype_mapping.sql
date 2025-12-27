9207fe2626914b512ca9a732a6c407b981c5c779
godbiao <57954259+bbiiaaoo@users.noreply.github.com>
2025-12-27 22:16:43 +0800

    Delete trino-connector directory

--- trino-connector/integration-test/src/test/resources/trino-ci-testset/testsets/jdbc-mysql/00009_datatype_mapping.sql
+++
@@ -1,4 +0,0 @@
--- Table create by integration-test-common/docker-script/init/mysql/init.sql
-show create table gt_mysql.gt_mysql_test_all_type.demo;
-
-select * from gt_mysql.gt_mysql_test_all_type.demo;
